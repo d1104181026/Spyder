d7ce4246c92f9f9b81e458831c7ac97b4b9aa524
vivi_lin_ <lin.hebe637@gmail.com>
2024-04-16 17:08:29 +0800

    測試

---
+++ display_image.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+img = cv2.imread( ".\dataset\Lena.pgm", -1 )
+cv2.imshow( "Example", img )
+cv2.waitKey( 0 )
+cv2.destroyAllWindows( )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
